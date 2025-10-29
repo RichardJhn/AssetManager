c0fd1852e6abdd2053057f8500e3481c15aec98e
RichardJhn <rjohn@my.yearupunited.org>
2025-10-29 10:45:10 -0400

    fixed prices

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -5,20 +5,25 @@ public class Main {
         Asset jewel = new Asset("June","Jewel",12000.00);
 
         ArrayList<Asset> assets = new ArrayList<>();
-        House h = new House("2010", "Mansion",1000000.00,"12321 Mcdonalds rd", 5 ,21213,13000);
-        Vehicle v = new Vehicle("2020", "Toyota", 10000.00,"Camry Sedan",5,24342);
+        House h = new House("2010", "Mansion",1000000.00,"12321 Mcdonalds rd", 1 ,21213,13000);
+        House h2 = new House("2020","My house",234232.00,"1232 My road",2,31233,123434);
+        Vehicle v = new Vehicle("2020", "Toyota", 10000.00,"Camry Sedan",2019,24342);
+        Vehicle v2 = new Vehicle("2021","Toyta",58000.00,"Supra",2018,124321);
 
         assets.add(jewel);
         assets.add(h);
         assets.add(v);
+        assets.add(h2);
+        assets.add(v2);
+
 
 
         for(Asset a : assets){
             System.out.println("----------------");
             System.out.println("Date Acquired: " + a.dateAcquired);
             System.out.println("Description: " + a.description);
-            System.out.println("Original Cost: " + a.originalCost);
-            System.out.println("Current Value: " + a.getValue());
+            System.out.printf("Original Cost: $%.2f \n", a.originalCost);
+            System.out.printf("Current Value: $%.2f \n", a.getValue());
 
         }
     }
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,6 +1,5 @@
 package com.pluralsight;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Vehicle extends  Asset{
     String makeModel;
@@ -51,25 +50,23 @@ public class Vehicle extends  Asset{
     public double getValue(){
         int currentYear = 2025;
         int age = currentYear - year;
-        double value = getOriginalCost();
-        List<Vehicle> vehicleList = new ArrayList<>();
-        for (Vehicle vehicle : vehicleList){
-            if (vehicle.year <=3){
-                System.out.printf("Your car value is %.2f", vehicle.originalCost * .97 );
+        double value;
+            if (age <=3){
+                return this.originalCost * Math.pow(.97,age);
             }
-            else if (vehicle.year >3 && vehicle.year <= 6){
-                System.out.printf("Your car value is %.2f", vehicle.originalCost * .94);
+            else if (age >3 && age <= 6){
+                return this.originalCost * Math.pow(.94,age);
             }
-            else if (vehicle.year > 6 && vehicle.year >=10){
-                System.out.printf("Your car value us %.2f", vehicle.originalCost * .92);
+            else if (age > 6 && age <=10){
+                return this.originalCost * Math.pow(.91,age);
             }
-            else if (vehicle.year > 10) {
-                System.out.println("Your car value is" + 1000.00 );
+            else if (age > 10) {
+                return 1000.00;
             }
             else {
                 System.out.println("error");
             }
-        }
-        return value;
+
+        return originalCost;
     }
 }
